22180df25cb018c332d3843c9df111efa7ee88d3
Eremiya Rifat <eremia2021@gmail.com>
2022-11-08 21:51:58 +0100

    Search-Bar vervollständigt

--- README.md
+++ README.md
@@ -5,3 +5,6 @@ Vorerst fange ich ich mit einer einfachen Version der Webseite an und versuche m
 Fokussieren mich jedoch nicht auf details und versuche das grobe hinzukriegen um mich so schnell wie möglich aufs Backend zu konzentrieren, da mich das sehr interessiert.
 
 ![alt text](./check24-holiday/src/images/screenshots/screenshot1.png "Logo Title Text 1")
+
+### 08.11 
+Heute habe ich wenig Zeit gehabt, also habe ich die Front-End Entwicklung und habe den Search-Bar fertig gemacht. Asl nächstes folgt ein kleinen Backend-Test
--- check24-holiday/src/SearchField.js
+++ check24-holiday/src/SearchField.js
@@ -11,13 +11,38 @@ function SearchField()
                     <div className="inputContainer">
                     <input className="input" type="text" placeholder="Alle Reiseziele zum entdecken" />
                     </div>
+
                     <div className="inputContainer">
                     <input className="input" type="text" placeholder="Deutschland - alle Flughäfen" />
                     </div>
+
                     <div className="inputContainer">
                     <input className="input" type="text" placeholder="Alle Reiseziele zum entdecken" />
-                    </div>
+                    </div> 
                 </div>
+  
+                <div className="searchField-horInput">
+                    <div className="inputContainer">
+                    <input className="input" type="text" placeholder="Reisedauer" />
+                    </div>
+
+                    <p>zwischen</p>
+
+                    <div className="inputContainer">
+                    <input className="input" type="text" placeholder="früheste Hinreise" />
+                    </div>
+                    <p>und</p>
+
+                    <div className="inputContainer">
+                    <input className="input" type="text" placeholder="späteste Hinreise" />
+                    </div> 
+
+                    <button className="searchField-button">Reise finden</button>
+                    
+                </div>
+
+
+
             </div>
             
         </div>
